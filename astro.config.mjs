b3878ee1797ff74dedbef5b05ce85be4dd2cb2e7
Huerta Murillo Alejandro Saul <saul901_@hotmail.com>
2025-03-25 21:47:26 -0600

    WIP: buttons

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,11 @@ import tailwindcss from "@tailwindcss/vite";
 
 import react from '@astrojs/react';
 
+import icon from 'astro-icon';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  integrations: [react(), icon()],
   vite: {
     plugins: [tailwindcss()],
   },
