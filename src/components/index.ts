3e21529e7b28c8188880b274b35d86888a6cad73
Huerta Murillo Alejandro Saul <saul901_@hotmail.com>
2025-03-27 14:01:53 -0600

    404 not found

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { BalatroBG } from "./BalatroBG";
 export { default as Button } from "./Button.astro";
 export { TiltedCard } from "./TiltedCard";
+export {default as WarningInfo} from "./WarningInfo.astro"
